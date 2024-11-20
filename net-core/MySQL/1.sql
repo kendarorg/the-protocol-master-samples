5f0d206d529bfcf786b1117a5114497f9cd59a42
edaros <github@kendar.org>
2024-11-20 22:40:44 +0100

    SQL fix

--- net-core/MySQL/1.sql
+++ net-core/MySQL/1.sql
@@ -3,11 +3,12 @@
 
 -- noinspection SqlDialectInspectionForFile
 
-CREATE TABLE task (
-    id INT NOT NULL AUTO_INCREMENT,
+CREATE TABLE IF NOT EXISTS task  (
+    id INT(11)  NOT NULL AUTO_INCREMENT,
     task_name VARCHAR(255) NOT NULL,
     priority VARCHAR(255) NOT NULL,
     status VARCHAR(255),
     notes TEXT,
     archive_date DATETIME,
+    PRIMARY KEY (`id`)
 );
