a6ab4c7bd880637f2b1376bdebc1507354bbe174
edaros <github@kendar.org>
2024-11-20 22:19:05 +0100

    Starting for real

--- net-core/Tpm/start.sh
+++ net-core/Tpm/start.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-wget https://github.com/kendarorg/the-protocol-master/releases/download/v2.0.0/protocol-runner.jar
-java -jar /protocol-runner.jar -cfg /settings.json
+wget --no-verbose https://github.com/kendarorg/the-protocol-master/releases/download/v2.0.0/protocol-runner.jar
+java -jar /protocol-runner.jar -cfg /settings.json -unattended
