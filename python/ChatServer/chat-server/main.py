bd0a68b990500d221be09aaf4bf6d90e46d9d658
edaros <github@kendar.org>
2024-12-16 10:59:22 +0100

    Moved for dockerfile

--- python/ChatServer/chat-server/main.py
+++ python/ChatServer/chat-server/main.py
@@ -6,7 +6,7 @@ from web.lib.bottleService import BottleService
 def main():
     ctx = ApplicationContext(web)
     bottleService: BottleService = ctx.resolve(BottleService)
-    bottleService.run(host='', port=8080, debug=True)
+    bottleService.run(host='', port=80, debug=True)
 
 
 if __name__ == '__main__':
