1ccb64200ccf5a4c164cca9c91352112b0b1ba69
edaros <github@kendar.org>
2024-12-16 11:04:41 +0100

    Dockerfile

--- python/ChatServer/chat-server/main.py
+++ python/ChatServer/chat-server/main.py
@@ -1,12 +1,19 @@
 import web
+import argparse
 from ApplicationContext import ApplicationContext
 from web.lib.bottleService import BottleService
 
 
 def main():
+    parser = argparse.ArgumentParser(description='Start the website')
+    parser.add_argument('--port', metavar='port', required=False,
+                        help='The listening port',default="8080")
+
+    args = parser.parse_args()
+    port = int(args.port)
     ctx = ApplicationContext(web)
     bottleService: BottleService = ctx.resolve(BottleService)
-    bottleService.run(host='', port=80, debug=True)
+    bottleService.run(host='', port=port, debug=True)
 
 
 if __name__ == '__main__':
