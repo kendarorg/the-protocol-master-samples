78133968a3db5b85d7808d62d9b15c4f1ef343b9
edaros <github@kendar.org>
2024-12-18 16:40:34 +0100

    added swagger to python

--- python/RestServer/rest-server/web/controllers/quotationController.py
+++ python/RestServer/rest-server/web/controllers/quotationController.py
@@ -15,47 +15,26 @@ from web.lib.decorators import qroute
 @component
 class MessagesController(Controller):
 
-    @qroute("/api/quotation")
+    @qroute("/api/quotation/symbols")
     def get_all(self):
-        data = list(Quotation.select().order_by(Quotation.priority.desc()).dicts())
+        data = list(Quotation.select(Quotation.symbol).group_by(Quotation.symbol).dicts())
         response.content_type = 'application/json'
         return to_json_with_iso_dates(data)
 
-    @qroute("/api/quotation", verb="POST")
-    def create(self):
-        current: Quotation = dict_to_model(Quotation,json.loads(request.body.getvalue().decode('utf-8')))
-        current.save()
+    @qroute("/api/quotation/quotes/<identifier>")
+    def get_by_id(self, identifier:str):
+        data = list(Quotation.select().
+                    where(Quotation.symbol==identifier).
+                    order_by(Quotation.date.desc()).dicts())
         response.content_type = 'application/json'
-        return to_json_with_iso_dates(model_to_dict(current))
+        return to_json_with_iso_dates(data)
 
-    @qroute("/api/quotation/<index>")
-    def get_by_id(self, index):
-        for item in Quotation.select().where(Quotation.id == index):
-            response.content_type = 'application/json'
+    @qroute("/api/quotation/quote/<identifier>")
+    def get_by_id(self, identifier: str):
+        data = list(Quotation.select().
+                    where(Quotation.symbol == identifier).
+                    order_by(Quotation.date.desc()).dicts())
+        response.content_type = 'application/json'
+        for item in data:
             return to_json_with_iso_dates(model_to_dict(item))
         abort(404, "Item not found.")
-
-    @qroute("/api/quotation/<index>", verb="PUT")
-    def update(self, index):
-        updated: Quotation = dict_to_model(Quotation,json.loads(request.body.getvalue().decode('utf-8')))
-        for old in Quotation.select().where(Quotation.id == index):
-            old.shortDescription = updated.shortDescription
-            old.description = updated.description
-            old.priority = updated.priority
-            old.done = updated.done
-            old.before = updated.before
-            old.save()
-            response.content_type = 'application/json'
-            return to_json_with_iso_dates(model_to_dict(old))
-        abort(404, "Item not found.")
-
-    @qroute("/api/quotation/<index>", verb="DELETE")
-    def delete_by_id(self, index):
-        try:
-            for item in Quotation.select().where(Quotation.id == index):
-                result = to_json_with_iso_dates(model_to_dict(item))
-                item.delete_instance()
-                response.content_type = 'application/json'
-                return result
-        except:
-            return ''
