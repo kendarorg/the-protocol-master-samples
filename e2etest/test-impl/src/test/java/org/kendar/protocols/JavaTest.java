6b67c3445c7b4c1be3e78dba04df5a3b0cb1994e
edaros <github@kendar.org>
2025-03-18 17:36:33 +0100

    Testing Java with testcontainers

--- e2etest/test-impl/src/test/java/org/kendar/protocols/JavaTest.java
+++ e2etest/test-impl/src/test/java/org/kendar/protocols/JavaTest.java
@@ -62,4 +62,80 @@ public class JavaTest extends BasicTest{
                 check(() -> !getPageSource().contains("\"OK\"")),
                 "Unreachable http://java-rest/api/status");
     }
+
+    @Test
+    void B_testNavigation() throws InterruptedException {
+        navigateTo("about:blank");
+        Sleeper.sleep(500);
+        navigateTo("http://java-rest/index.html");//itemUpdateMETA
+        Sleeper.sleep(6000);
+
+//        alertWhenHumanDriven("Inserting new task");
+//        //Insert item
+//        fillItem("taskName", "Laundry");
+//        selectItem("addPriority", "High");
+//        fillItem("notes", "Wash Cotton");
+//
+//        //Submit
+//        clickItem("submitNewTask");
+//        Sleeper.sleep(1000);
+//        takeSnapshot();
+//        alertWhenHumanDriven("Inserting new task");
+//
+//        //Find the selected item
+//
+//        assertNotNull(findElementByXPath("//tbody[@id='listTableBody']/tr/td[contains(text(), \"Laundry\")]"));
+//        assertNotNull(findElementByXPath("//tbody[@id='listTableBody']/tr/td[contains(text(), \"Wash Cotton\")]"));
+//        takeSnapshot();
+//
+//
+//
+//        alertWhenHumanDriven("Completing task");
+//        //Modify the status
+//        var status = findElementsByXPath("//tbody[@id='listTableBody']/tr/td/select");
+//        selectItem(status.get(1), "Completed");
+//        Sleeper.sleep(1000);
+//        takeSnapshot();
+//
+//
+//        //Update
+//        var button = findElementsByXPath("//tbody[@id='listTableBody']/tr/td/button").get(1);
+//        button.click();
+//        Sleeper.sleep(1000);
+//        takeSnapshot();
+//
+//
+//        alertWhenHumanDriven("Archiving the task");
+//        //Reload the stale element
+//
+//        button = findElementsByXPath("//tbody[@id='listTableBody']/tr/td/button").get(1);
+//        button.click();
+//        Sleeper.sleep(1000);
+//        takeSnapshot();
+//
+//        //Set the archive
+//
+//        clickItem("setArchivedTasks");
+//        Sleeper.sleep(2000);
+//        takeSnapshot();
+//
+//
+//        alertWhenHumanDriven("Clean up the task");
+//        //Delete old item
+//
+//        button = findElementByXPath("//tbody[@id='archivedTableBody']/tr/td/button");
+//        button.click();
+//        Sleeper.sleep(1000);
+//        var yesButton = findElementByXPath(".//button[contains(text(), \"Yes, delete it!\")]");
+//        yesButton.click();
+//        Sleeper.sleep(1000);
+//
+//        var okButton = findElementByXPath(".//button[contains(text(), \"OK\")]");
+//        okButton.click();
+//        Sleeper.sleep(1000);
+//
+//        alertWhenHumanDriven("Operation completed");
+//        navigateTo("about:blank");
+    }
+
 }
