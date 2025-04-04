c39958457967d90968a3927fc8196491154b5a3b
edaros <github@kendar.org>
2025-04-04 11:43:47 +0200

    Added delete quotations on python project

--- e2etest/test-impl/src/test/java/org/kendar/protocols/PyTest.java
+++ e2etest/test-impl/src/test/java/org/kendar/protocols/PyTest.java
@@ -95,8 +95,6 @@ public class PyTest extends BasicTest{
             replayWithoutContainer("py-quote-generator");
 
             sendFakeMessages();
-
-            replayWithoutContainer("py-rabbit");
         }catch(Exception ex){
             System.out.println(ex.getMessage());
             throw new RuntimeException(ex);
